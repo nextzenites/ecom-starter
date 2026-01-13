6c364f48e7b79085a4c3af71472049101fbc0682
nextzenites <team.nextzenites@gmail.com>
2026-01-13 19:54:08 +0600

    Carousel and Product design

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,25 @@
+
+const ProductCard = ({ product }) => {
+    return (
+        <div className="w-60 h-80 bg-green-100 p-3 flex flex-col gap-1 rounded-2xl">
+            <div className="h-48 bg-gray-700 rounded-xl" >
+                {/* <img
+                    alt={product.name}
+                    src={product.image}
+                /> */}
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-row justify-between">
+                    <div className="flex flex-col">
+                        <span className="text-xl font-bold">{product.name}</span>
+                        <p className="text-xs text-gray-700">ID: {product.id}</p>
+                    </div>
+                    <span className="font-bold text-green-600 text-2xl">${product.price}</span>
+                </div>
+                <button className="hover:bg-green-700 text-gray-50 bg-green-800 py-2 rounded-md">Add to cart</button>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
